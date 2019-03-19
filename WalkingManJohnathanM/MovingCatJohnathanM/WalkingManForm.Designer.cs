7337f8860406341786e2145262b3cd652e83dd83
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-03-19 13:29:34 -0400

    Started Walking Man

---
+++ WalkingManJohnathanM/MovingCatJohnathanM/WalkingManForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MovingCatJohnathanM
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMovingCat));
+            this.picZombie = new System.Windows.Forms.PictureBox();
+            this.btnForwards = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnRunForever = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picZombie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picZombie
+            // 
+            this.picZombie.BackColor = System.Drawing.Color.Transparent;
+            this.picZombie.Image = global::MovingCatJohnathanM.Properties.Resources.zombie1;
+            this.picZombie.InitialImage = null;
+            this.picZombie.Location = new System.Drawing.Point(64, 31);
+            this.picZombie.Name = "picZombie";
+            this.picZombie.Size = new System.Drawing.Size(192, 193);
+            this.picZombie.TabIndex = 0;
+            this.picZombie.TabStop = false;
+            this.picZombie.Click += new System.EventHandler(this.picZombie_Click);
+            // 
+            // btnForwards
+            // 
+            this.btnForwards.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnForwards.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnForwards.Font = new System.Drawing.Font("Tekton Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForwards.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnForwards.Location = new System.Drawing.Point(278, 43);
+            this.btnForwards.Name = "btnForwards";
+            this.btnForwards.Size = new System.Drawing.Size(158, 33);
+            this.btnForwards.TabIndex = 2;
+            this.btnForwards.Text = "Slow Motion Run";
+            this.btnForwards.UseVisualStyleBackColor = false;
+            this.btnForwards.Click += new System.EventHandler(this.btnForwards_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRun.Font = new System.Drawing.Font("Tekton Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRun.Location = new System.Drawing.Point(278, 116);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(158, 33);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run Once";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnRunForever
+            // 
+            this.btnRunForever.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRunForever.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRunForever.Font = new System.Drawing.Font("Tekton Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRunForever.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRunForever.Location = new System.Drawing.Point(278, 191);
+            this.btnRunForever.Name = "btnRunForever";
+            this.btnRunForever.Size = new System.Drawing.Size(158, 33);
+            this.btnRunForever.TabIndex = 4;
+            this.btnRunForever.Text = "Run Forever";
+            this.btnRunForever.UseVisualStyleBackColor = false;
+            this.btnRunForever.Click += new System.EventHandler(this.btnRunForever_Click);
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(457, 264);
+            this.Controls.Add(this.btnRunForever);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnForwards);
+            this.Controls.Add(this.picZombie);
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat by Johnathan M";
+            this.Load += new System.EventHandler(this.frmMovingCat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picZombie)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox picZombie;
+        public System.Windows.Forms.Button btnForwards;
+        public System.Windows.Forms.Button btnRun;
+        public System.Windows.Forms.Button btnRunForever;
+    }
+}
+
---
+++ WalkingManJohnathanM/MovingCatJohnathanM/WalkingManForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Created by: Johnathan Meloche
+ * Created on: 19/03/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 Walking Man
+ * This program cycles through animation frames until it reaches max frames
+*/
+namespace MovingCatJohnathanM
+{
+    public partial class frmMovingCat : Form
+    {
+        // global variables
+        int zombieCount = 1;
+        int zombieRunCount = 1;
+        int zombieCycleCount = 1;
+        const int MAX_FRAMES = 10;
+
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+            
+        private void btnForwards_Click(object sender, EventArgs e)
+        {
+            
+            if (zombieCount == 1)
+            {
+                zombieCount = 2;
+
+                this.picZombie.Load(@"Images\zombie2.png");
+
+            }
+            else if (zombieCount == 2)
+            {
+                zombieCount = 3;
+
+                this.picZombie.Load(@"Images\zombie3.png");
+            }
+            else if (zombieCount == 3)
+            {
+                zombieCount = 4;
+
+                this.picZombie.Load(@"Images\zombie4.png");
+            }
+            else if (zombieCount == 4)
+            {
+                zombieCount = 5;
+
+                this.picZombie.Load(@"Images\zombie5.png");
+            }
+            else if (zombieCount == 5)
+            {
+                zombieCount = 6;
+
+                this.picZombie.Load(@"Images\zombie6.png");
+            }
+            else if (zombieCount == 6)
+            {
+                zombieCount = 7;
+
+                this.picZombie.Load(@"Images\zombie7.png");
+            }
+            else if (zombieCount == 7)
+            {
+                zombieCount = 8;
+
+                this.picZombie.Load(@"Images\zombie8.png");
+            }
+            else if (zombieCount == 8)
+            {
+                zombieCount = 9;
+
+                this.picZombie.Load(@"Images\zombie9.png");
+            }
+            else if (zombieCount == 9)
+            {
+                zombieCount = 10;
+
+                this.picZombie.Load(@"Images\zombie10.png");
+            }
+            else if (zombieCount == 10)
+            {
+                zombieCount = 1;
+
+                this.picZombie.Load(@"Images\zombie1.png");
+            }
+           
+        }
+
+        private void picZombie_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void frmMovingCat_Load(object sender, EventArgs e)
+        {
+            zombieCount = 1;
+            zombieCycleCount = 1;
+            zombieRunCount = 1;
+            this.picZombie.Load(@"Images\zombie1.png");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            while (zombieRunCount < MAX_FRAMES + 1)
+            {
+                if (zombieRunCount == 1)
+                {
+                    zombieRunCount = 2;
+
+                    this.picZombie.Load(@"Images\zombie2.png");
+                }
+                else if (zombieRunCount == 2)
+                {
+                    zombieRunCount = 3;
+
+                    this.picZombie.Load(@"Images\zombie3.png");
+                }
+                else if (zombieRunCount == 3)
+                {
+                    zombieRunCount = 4;
+
+                    this.picZombie.Load(@"Images\zombie4.png");
+                }
+                else if (zombieRunCount == 4)
+                {
+                    zombieRunCount = 5;
+
+                    this.picZombie.Load(@"Images\zombie5.png");
+                }
+                else if (zombieRunCount == 5)
+                {
+                    zombieRunCount = 6;
+
+                    this.picZombie.Load(@"Images\zombie6.png");
+                }
+                else if (zombieRunCount == 6)
+                {
+                    zombieRunCount = 7;
+
+                    this.picZombie.Load(@"Images\zombie7.png");
+                }
+                else if (zombieRunCount == 7)
+                {
+                    zombieRunCount = 8;
+
+                    this.picZombie.Load(@"Images\zombie8.png");
+                }
+                else if (zombieRunCount == 8)
+                {
+                    zombieRunCount = 9;
+
+                    this.picZombie.Load(@"Images\zombie9.png");
+                }
+                else if (zombieRunCount == 9)
+                {
+                    zombieRunCount = 10;
+
+                    this.picZombie.Load(@"Images\zombie10.png");
+                }
+            }
+            
+        }
+
+        private void btnRunForever_Click(object sender, EventArgs e)
+        {
+            while (zombieCycleCount < MAX_FRAMES + 1)
+            {
+                if (zombieCycleCount == 1)
+                {
+                    zombieCycleCount = 2;
+
+                    this.picZombie.Load(@"Images\zombie2.png");
+                }
+                else if (zombieCycleCount == 2)
+                {
+                    zombieCycleCount = 3;
+
+                    this.picZombie.Load(@"Images\zombie3.png");
+                }
+                else if (zombieCycleCount == 3)
+                {
+                    zombieCycleCount = 4;
+
+                    this.picZombie.Load(@"Images\zombie4.png");
+                }
+                else if (zombieCycleCount == 4)
+                {
+                    zombieCycleCount = 5;
+
+                    this.picZombie.Load(@"Images\zombie5.png");
+                }
+                else if (zombieCycleCount == 5)
+                {
+                    zombieCycleCount = 6;
+
+                    this.picZombie.Load(@"Images\zombie6.png");
+                }
+                else if (zombieCycleCount == 6)
+                {
+                    zombieCycleCount = 7;
+
+                    this.picZombie.Load(@"Images\zombie7.png");
+                }
+                else if (zombieCycleCount == 7)
+                {
+                    zombieCycleCount = 8;
+
+                    this.picZombie.Load(@"Images\zombie8.png");
+                }
+                else if (zombieCycleCount == 8)
+                {
+                    zombieCycleCount = 9;
+
+                    this.picZombie.Load(@"Images\zombie9.png");
+                }
+                else if (zombieCycleCount == 9)
+                {
+                    zombieCycleCount = 10;
+
+                    this.picZombie.Load(@"Images\zombie10.png");
+
+                    this.Refresh();
+                }
+            }
+        }
+    }
+}
