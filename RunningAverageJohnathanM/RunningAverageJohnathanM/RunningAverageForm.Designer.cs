b735d03d4b019b104171ccb131a8dc46320f3076
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-04-03 13:26:22 -0400

    Start Day 22-29

---
+++ RunningAverageJohnathanM/RunningAverageJohnathanM/RunningAverageForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RunningAverageJohnathanM
+{
+    partial class RunningAverageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RunningAverageForm));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Tekton Pro", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(103, 293);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(154, 46);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Average";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnRestart.Font = new System.Drawing.Font("Tekton Pro", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(446, 293);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(154, 46);
+            this.btnRestart.TabIndex = 1;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.BackColor = System.Drawing.Color.Transparent;
+            this.lblPrompt.Font = new System.Drawing.Font("Tekton Pro", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrompt.ForeColor = System.Drawing.Color.Turquoise;
+            this.lblPrompt.Location = new System.Drawing.Point(70, 91);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(254, 75);
+            this.lblPrompt.TabIndex = 2;
+            this.lblPrompt.Text = "Input a number and then\r\nclick the button to calculate\r\nthe average";
+            this.lblPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Tekton Pro", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(482, 157);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(26, 25);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "0";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Tekton Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(134, 200);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 36);
+            this.txtInput.TabIndex = 4;
+            // 
+            // RunningAverageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(687, 455);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "RunningAverageForm";
+            this.Text = "Running Average by Johnathan M";
+            this.Load += new System.EventHandler(this.RunningAverageForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ RunningAverageJohnathanM/RunningAverageJohnathanM/RunningAverageForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Created by: Johnathan Meloche
+ * Created on: 03/04/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Running Average
+ * This Program Adds input and calculates the average
+*/
+
+namespace RunningAverageJohnathanM
+{
+    public partial class RunningAverageForm : Form
+    {
+        public RunningAverageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RunningAverageForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        Double NumberSet = 0;
+        Double NumberCounter = 0;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            NumberCounter++;
+            NumberSet = NumberSet + Convert.ToDouble(txtInput.Text);
+            lblAnswer.Text = Convert.ToString(NumberSet / NumberCounter);
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
